ca54b6fda702be4cbe949cbf521d929a112ab2ed
ronknight <webmaster@pinoyitsolution.com>
2021-03-10 15:15:40 -0800

    Edit footer.php to display dynamic company name.

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -3,7 +3,7 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 0.1.0
     </div>
-    <strong>Copyright &copy; ABC Inc. 2018-<?php echo date('Y') ?>.</strong> All rights reserved.
+    <strong>Copyright &copy; <?php echo $company_data['company_name'] ?> 2018-<?php echo date('Y') ?>.</strong> All rights reserved.
   </footer>
 
   <!-- Add the sidebar's background. This div must be placed
