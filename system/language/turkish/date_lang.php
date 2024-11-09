10c649cf0cdd29f18f35b0484695dba5c330d327
Ali Çömez | Slaweally <sys@rootali.net>
2024-11-09 22:26:10 +0300

    Create date_lang.php

---
+++ system/language/turkish/date_lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * PHP için açık kaynak uygulama geliştirme çerçevesi
+ *
+ * Bu içerik MIT Lisansı (MIT) altında yayınlanmıştır
+ *
+ * Telif Hakkı (c) 2014 - 2017, British Columbia Institute of Technology
+ *
+ * Bu yazılımın ve ilgili dokümantasyon dosyalarının (Yazılım) bir kopyasını
+ * ücretsiz olarak elde eden herhangi bir kişiye, sınırlama olmaksızın
+ * Yazılım'ı kullanma, kopyalama, değiştirme, birleştirme, yayımlama, dağıtma,
+ * alt lisanslama ve/veya Yazılım'ın kopyalarını satma hakkı verilmiştir ve
+ * Yazılım'ın sağlandığı kişilere aşağıdaki koşullar altında bunu yapmalarına
+ * izin verilir:
+ *
+ * Yukarıdaki telif hakkı bildirimi ve bu izin bildirimi Yazılım'ın tüm kopyalarına
+ * veya önemli bölümlerine dahil edilmelidir.
+ *
+ * YAZILIM, SATILABİLİRLİK, BELİRLİ BİR AMACA UYGUNLUK VE İHLAL EDİLMEMESİ DAHİL
+ * ANCAK BUNLARLA SINIRLI OLMAMAK ÜZERE, HERHANGİ BİR TÜRDE GARANTİ OLMAKSIZIN
+ * "OLDUĞU GİBİ" SAĞLANIR. HİÇBİR DURUMDA YAZARLAR VEYA TELİF HAKKI SAHİPLERİ,
+ * YAZILIM VEYA YAZILIM'IN KULLANIMI VEYA DİĞER İŞLEMLERLE İLGİLİ, SÖZLEŞME, HAKSIZ
+ * FİİL VEYA BAŞKA BİR DURUMDA HERHANGİ BİR İDDİA, HASAR VEYA DİĞER SORUMLULUKLARDAN
+ * SORUMLU OLMAYACAKTIR.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Geliştirici Ekibi
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2017, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT Lisansı
+ * @link	https://codeigniter.com
+ * @since	Sürüm 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('Doğrudan erişime izin verilmez');
+
+$lang['date_year'] = 'Yıl';
+$lang['date_years'] = 'Yıllar';
+$lang['date_month'] = 'Ay';
+$lang['date_months'] = 'Aylar';
+$lang['date_week'] = 'Hafta';
+$lang['date_weeks'] = 'Haftalar';
+$lang['date_day'] = 'Gün';
+$lang['date_days'] = 'Günler';
+$lang['date_hour'] = 'Saat';
+$lang['date_hours'] = 'Saatler';
+$lang['date_minute'] = 'Dakika';
+$lang['date_minutes'] = 'Dakikalar';
+$lang['date_second'] = 'Saniye';
+$lang['date_seconds'] = 'Saniyeler';
+
+$lang['UM12']	= '(UTC -12:00) Baker/Howland Adası';
+$lang['UM11']	= '(UTC -11:00) Niue';
+$lang['UM10']	= '(UTC -10:00) Hawaii-Aleutian Standart Saati, Cook Adaları, Tahiti';
+$lang['UM95']	= '(UTC -9:30) Marquesas Adaları';
+$lang['UM9']	= '(UTC -9:00) Alaska Standart Saati, Gambier Adaları';
+$lang['UM8']	= '(UTC -8:00) Pasifik Standart Saati, Clipperton Adası';
+$lang['UM7']	= '(UTC -7:00) Dağ Standart Saati';
+$lang['UM6']	= '(UTC -6:00) Merkez Standart Saati';
+$lang['UM5']	= '(UTC -5:00) Doğu Standart Saati, Batı Karayip Standart Saati';
+$lang['UM45']	= '(UTC -4:30) Venezuela Standart Saati';
+$lang['UM4']	= '(UTC -4:00) Atlantik Standart Saati, Doğu Karayip Standart Saati';
+$lang['UM35']	= '(UTC -3:30) Newfoundland Standart Saati';
+$lang['UM3']	= '(UTC -3:00) Arjantin, Brezilya, Fransız Guyanası, Uruguay';
+$lang['UM2']	= '(UTC -2:00) Güney Georgia/Güney Sandwich Adaları';
+$lang['UM1']	= '(UTC -1:00) Azorlar, Cape Verde Adaları';
+$lang['UTC']	= '(UTC) Greenwich Ortalama Saati, Batı Avrupa Saati';
+$lang['UP1']	= '(UTC +1:00) Orta Avrupa Saati, Batı Afrika Saati';
+$lang['UP2']	= '(UTC +2:00) Orta Afrika Saati, Doğu Avrupa Saati, Kaliningrad Saati';
+$lang['UP3']	= '(UTC +3:00) Moskova Saati, Doğu Afrika Saati, Arabistan Standart Saati';
+$lang['UP35']	= '(UTC +3:30) İran Standart Saati';
+$lang['UP4']	= '(UTC +4:00) Azerbaycan Standart Saati, Samara Saati';
+$lang['UP45']	= '(UTC +4:30) Afganistan';
+$lang['UP5']	= '(UTC +5:00) Pakistan Standart Saati, Yekaterinburg Saati';
+$lang['UP55']	= '(UTC +5:30) Hindistan Standart Saati, Sri Lanka Saati';
+$lang['UP575']	= '(UTC +5:45) Nepal Saati';
+$lang['UP6']	= '(UTC +6:00) Bangladeş Standart Saati, Bhutan Saati, Omsk Saati';
+$lang['UP65']	= '(UTC +6:30) Cocos Adaları, Myanmar';
+$lang['UP7']	= '(UTC +7:00) Krasnoyarsk Saati, Kamboçya, Laos, Tayland, Vietnam';
+$lang['UP8']	= '(UTC +8:00) Avustralya Batı Standart Saati, Pekin Saati, Irkutsk Saati';
+$lang['UP875']	= '(UTC +8:45) Avustralya Orta Batı Standart Saati';
+$lang['UP9']	= '(UTC +9:00) Japonya Standart Saati, Kore Standart Saati, Yakutsk Saati';
+$lang['UP95']	= '(UTC +9:30) Avustralya Orta Standart Saati';
+$lang['UP10']	= '(UTC +10:00) Avustralya Doğu Standart Saati, Vladivostok Saati';
+$lang['UP105']	= '(UTC +10:30) Lord Howe Adası';
+$lang['UP11']	= '(UTC +11:00) Srednekolymsk Saati, Solomon Adaları, Vanuatu';
+$lang['UP115']	= '(UTC +11:30) Norfolk Adası';
+$lang['UP12']	= '(UTC +12:00) Fiji, Gilbert Adaları, Kamchatka Saati, Yeni Zelanda Standart Saati';
+$lang['UP1275']	= '(UTC +12:45) Chatham Adaları Standart Saati';
+$lang['UP13']	= '(UTC +13:00) Samoa Zaman Dilimi, Phoenix Adaları Saati, Tonga';
+$lang['UP14']	= '(UTC +14:00) Line Adaları';
