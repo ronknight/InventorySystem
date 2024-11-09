f14b17ebb38c3e8afbaea678c21a81a56feadeda
Ali Çömez | Slaweally <sys@rootali.net>
2024-11-09 22:33:41 +0300

    Create ftp_lang.php

---
+++ system/language/turkish/ftp_lang.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * PHP için açık kaynak uygulama geliştirme çerçevesi
+ *
+ * Bu içerik MIT Lisansı (MIT) altında yayınlanmıştır
+ *
+ * Telif Hakkı (c) 2014 - 2017, British Columbia Institute of Technology
+ *
+ * Bu yazılımın ve ilgili dokümantasyon dosyalarının (Yazılım) bir kopyasını
+ * ücretsiz olarak elde eden herhangi bir kişiye, sınırlama olmaksızın
+ * Yazılım'ı kullanma, kopyalama, değiştirme, birleştirme, yayımlama, dağıtma,
+ * alt lisanslama ve/veya Yazılım'ın kopyalarını satma hakkı verilmiştir ve
+ * Yazılım'ın sağlandığı kişilere aşağıdaki koşullar altında bunu yapmalarına
+ * izin verilir:
+ *
+ * Yukarıdaki telif hakkı bildirimi ve bu izin bildirimi Yazılım'ın tüm kopyalarına
+ * veya önemli bölümlerine dahil edilmelidir.
+ *
+ * YAZILIM, SATILABİLİRLİK, BELİRLİ BİR AMACA UYGUNLUK VE İHLAL EDİLMEMESİ DAHİL
+ * ANCAK BUNLARLA SINIRLI OLMAMAK ÜZERE, HERHANGİ BİR TÜRDE GARANTİ OLMAKSIZIN
+ * "OLDUĞU GİBİ" SAĞLANIR. HİÇBİR DURUMDA YAZARLAR VEYA TELİF HAKKI SAHİPLERİ,
+ * YAZILIM VEYA YAZILIM'IN KULLANIMI VEYA DİĞER İŞLEMLERLE İLGİLİ, SÖZLEŞME, HAKSIZ
+ * FİİL VEYA BAŞKA BİR DURUMDA HERHANGİ BİR İDDİA, HASAR VEYA DİĞER SORUMLULUKLARDAN
+ * SORUMLU OLMAYACAKTIR.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Geliştirici Ekibi
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2017, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT Lisansı
+ * @link	https://codeigniter.com
+ * @since	Sürüm 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('Doğrudan erişime izin verilmez');
+
+$lang['ftp_no_connection']		= 'Geçerli bir bağlantı ID\'si bulunamıyor. Lütfen dosya işlemlerini gerçekleştirmeden önce bağlı olduğunuzdan emin olun.';
+$lang['ftp_unable_to_connect']		= 'Verilen sunucu adıyla FTP sunucunuza bağlanılamıyor.';
+$lang['ftp_unable_to_login']		= 'FTP sunucunuza giriş yapılamıyor. Lütfen kullanıcı adınızı ve şifrenizi kontrol edin.';
+$lang['ftp_unable_to_mkdir']		= 'Belirttiğiniz dizin oluşturulamıyor.';
+$lang['ftp_unable_to_changedir']	= 'Dizinler değiştirilemiyor.';
+$lang['ftp_unable_to_chmod']		= 'Dosya izinleri ayarlanamıyor. Lütfen yolunuzu kontrol edin.';
+$lang['ftp_unable_to_upload']		= 'Belirtilen dosya yüklenemiyor. Lütfen yolunuzu kontrol edin.';
+$lang['ftp_unable_to_download']		= 'Belirtilen dosya indirilemiyor. Lütfen yolunuzu kontrol edin.';
+$lang['ftp_no_source_file']		= 'Kaynak dosya bulunamıyor. Lütfen yolunuzu kontrol edin.';
+$lang['ftp_unable_to_rename']		= 'Dosya yeniden adlandırılamıyor.';
+$lang['ftp_unable_to_delete']		= 'Dosya silinemiyor.';
+$lang['ftp_unable_to_move']		= 'Dosya taşınamıyor. Lütfen hedef dizinin mevcut olduğundan emin olun.';
