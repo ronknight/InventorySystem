41f643fb5c6acbc4d917e702049d70854ec0dc2b
Ali Çömez | Slaweally <sys@rootali.net>
2024-11-09 22:43:20 +0300

    Create unit_test_lang.php

---
+++ system/language/turkish/unit_test_lang.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * PHP için açık kaynak uygulama geliştirme çerçevesi
+ *
+ * Bu içerik MIT Lisansı (MIT) altında yayınlanmıştır
+ *
+ * Telif Hakkı (c) 2014 - 2017, British Columbia Institute of Technology
+ *
+ * Bu yazılımın ve ilgili dokümantasyon dosyalarının (Yazılım) bir kopyasını
+ * ücretsiz olarak elde eden herhangi bir kişiye, sınırlama olmaksızın
+ * Yazılım'ı kullanma, kopyalama, değiştirme, birleştirme, yayımlama, dağıtma,
+ * alt lisanslama ve/veya Yazılım'ın kopyalarını satma hakkı verilmiştir ve
+ * Yazılım'ın sağlandığı kişilere aşağıdaki koşullar altında bunu yapmalarına
+ * izin verilir:
+ *
+ * Yukarıdaki telif hakkı bildirimi ve bu izin bildirimi Yazılım'ın tüm kopyalarına
+ * veya önemli bölümlerine dahil edilmelidir.
+ *
+ * YAZILIM, SATILABİLİRLİK, BELİRLİ BİR AMACA UYGUNLUK VE İHLAL EDİLMEMESİ DAHİL
+ * ANCAK BUNLARLA SINIRLI OLMAMAK ÜZERE, HERHANGİ BİR TÜRDE GARANTİ OLMAKSIZIN
+ * "OLDUĞU GİBİ" SAĞLANIR. HİÇBİR DURUMDA YAZARLAR VEYA TELİF HAKKI SAHİPLERİ,
+ * YAZILIM VEYA YAZILIM'IN KULLANIMI VEYA DİĞER İŞLEMLERLE İLGİLİ, SÖZLEŞME, HAKSIZ
+ * FİİL VEYA BAŞKA BİR DURUMDA HERHANGİ BİR İDDİA, HASAR VEYA DİĞER SORUMLULUKLARDAN
+ * SORUMLU OLMAYACAKTIR.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Geliştirici Ekibi
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2017, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT Lisansı
+ * @link	https://codeigniter.com
+ * @since	Sürüm 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('Doğrudan erişime izin verilmez');
+
+$lang['ut_test_name'] = 'Test Adı';
+$lang['ut_test_datatype'] = 'Test Veri Tipi';
+$lang['ut_res_datatype'] = 'Beklenen Veri Tipi';
+$lang['ut_result'] = 'Sonuç';
+$lang['ut_undefined'] = 'Tanımsız Test Adı';
+$lang['ut_file'] = 'Dosya Adı';
+$lang['ut_line'] = 'Satır Numarası';
+$lang['ut_passed'] = 'Başarılı';
+$lang['ut_failed'] = 'Başarısız';
+$lang['ut_boolean'] = 'Boolean';
+$lang['ut_integer'] = 'Tamsayı';
+$lang['ut_float'] = 'Float';
+$lang['ut_double'] = 'Float'; // float ile aynı olabilir
+$lang['ut_string'] = 'Dize';
+$lang['ut_array'] = 'Dizi';
+$lang['ut_object'] = 'Nesne';
+$lang['ut_resource'] = 'Kaynak';
+$lang['ut_null'] = 'Null';
+$lang['ut_notes'] = 'Notlar';
