fb91d405a01481d54ed72309767cd2e5f0072710
Ali Çömez | Slaweally <sys@rootali.net>
2024-11-09 22:40:36 +0300

    Create pagination_lang.php

---
+++ system/language/turkish/pagination_lang.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * PHP için açık kaynak uygulama geliştirme çerçevesi
+ *
+ * Bu içerik MIT Lisansı (MIT) altında yayınlanmıştır
+ *
+ * Telif Hakkı (c) 2014 - 2017, British Columbia Institute of Technology
+ *
+ * Bu yazılımın ve ilgili dokümantasyon dosyalarının (Yazılım) bir kopyasını
+ * ücretsiz olarak elde eden herhangi bir kişiye, sınırlama olmaksızın
+ * Yazılım'ı kullanma, kopyalama, değiştirme, birleştirme, yayımlama, dağıtma,
+ * alt lisanslama ve/veya Yazılım'ın kopyalarını satma hakkı verilmiştir ve
+ * Yazılım'ın sağlandığı kişilere aşağıdaki koşullar altında bunu yapmalarına
+ * izin verilir:
+ *
+ * Yukarıdaki telif hakkı bildirimi ve bu izin bildirimi Yazılım'ın tüm kopyalarına
+ * veya önemli bölümlerine dahil edilmelidir.
+ *
+ * YAZILIM, SATILABİLİRLİK, BELİRLİ BİR AMACA UYGUNLUK VE İHLAL EDİLMEMESİ DAHİL
+ * ANCAK BUNLARLA SINIRLI OLMAMAK ÜZERE, HERHANGİ BİR TÜRDE GARANTİ OLMAKSIZIN
+ * "OLDUĞU GİBİ" SAĞLANIR. HİÇBİR DURUMDA YAZARLAR VEYA TELİF HAKKI SAHİPLERİ,
+ * YAZILIM VEYA YAZILIM'IN KULLANIMI VEYA DİĞER İŞLEMLERLE İLGİLİ, SÖZLEŞME, HAKSIZ
+ * FİİL VEYA BAŞKA BİR DURUMDA HERHANGİ BİR İDDİA, HASAR VEYA DİĞER SORUMLULUKLARDAN
+ * SORUMLU OLMAYACAKTIR.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Geliştirici Ekibi
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2017, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT Lisansı
+ * @link	https://codeigniter.com
+ * @since	Sürüm 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('Doğrudan erişime izin verilmez');
+
+$lang['pagination_first_link'] = '&lsaquo; İlk';
+$lang['pagination_next_link'] = '&gt;';
+$lang['pagination_prev_link'] = '&lt;';
+$lang['pagination_last_link'] = 'Son &rsaquo;';
